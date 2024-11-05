2e6711d3c7a6c6b84ae14f872eb4e6b0aefefe01
Hypedkratos <shashianand8651@gmail.com>
2024-11-05 22:31:27 +0530

    qwertyqwertyqwertywertyqwertyqwerty

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -12,7 +12,7 @@ const Avatar = ({ img, size = "md" }: AvatarProps) => (
       myStyles[`app-avatar-${size}`]
     }`}
   >
-    <img src={img as string} alt="null-is-null-and-null" />
+    <img src={img as string} alt="null-is-null-and-null-but-null" />
   </div>
 );
 
